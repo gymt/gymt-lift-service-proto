1f9ca869a1dab6b860fc8218fc31250336254839
Eric Lee <ayrocklee@gmail.com>
2019-03-29 14:51:31 -0500

    Refactored into multiple project for IoC. Added SimpleInjector.

--- LiftService.Program/Program.cs
+++ LiftService.Program/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace gymt_lift_service_proto
+namespace LiftService.Program
 {
     public class Program
     {
---
+++ LiftService.Program/Startup.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+using SimpleInjector;
+using SimpleInjector.Lifestyles;
+using SimpleInjector.Integration.AspNetCore.Mvc;
+using System.Reflection;
+
+using LiftService.Controller.Controllers;
+
+namespace LiftService.Program
+{
+    public class Startup
+    {
+        private Container container = new Container();
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var builder = services.AddMvcCore();
+            builder.AddAuthorization();
+            builder.AddFormatterMappings();
+            builder.AddJsonFormatters();
+            builder.AddCors();
+            builder.AddApplicationPart(typeof(LiftController).GetTypeInfo().Assembly).AddControllersAsServices();
+            builder.SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            IntegrateSimpleInjector(services);
+        }
+        
+        private void IntegrateSimpleInjector(IServiceCollection services)
+        {
+            container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
+
+            services.AddHttpContextAccessor();
+
+            services.AddSingleton<IControllerActivator>(
+                new SimpleInjectorControllerActivator(container));
+
+            services.EnableSimpleInjectorCrossWiring(container);
+            services.UseSimpleInjectorAspNetRequestScoping(container);
+        }
+        
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            InitializeContainer(app);
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+        
+        private void InitializeContainer(IApplicationBuilder app)
+        {
+            container.RegisterMvcControllers(app);
+
+            // Add application services. For instance:
+            // container.Register<IUserService, UserService>(Lifestyle.Scoped);
+
+            container.AutoCrossWireAspNetComponents(app);
+        }
+        
+    }
+}
