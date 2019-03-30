7eeb39703dfebbe84cd5337de364f179af1bbefb
Justin Johnson <jjustin634@gmail.com>
2019-03-29 23:02:44 -0500

    Registering the database context for a in memory database

---
+++ LiftService.Domain/Model/Lift.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LiftService.Domain.Model
+{
+    public class Lift
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
---
+++ LiftService.Domain/Model/LiftContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LiftService.Domain.Model
+{
+    public class LiftContext : DbContext
+    {
+        public LiftContext(DbContextOptions<LiftContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Lift> Lifts { get; set; }
+    }
+}
