7534b40d6381df1eaa485202d6bf2295d1080cbf
Eric Lee <ayrocklee@gmail.com>
2019-04-30 19:06:15 -0500

    add a restricted resource endpoint

--- LiftService.Controller/Controllers/LiftController.cs
+++ LiftService.Controller/Controllers/LiftController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using LiftService.Domain.Model;
@@ -12,49 +13,15 @@ namespace LiftService.Controller.Controllers
     [ApiController]
     public class LiftController : ControllerBase
     {
-        private readonly GymtContext _db;
-
-        public LiftController(GymtContext db)
+        public LiftController()
         {
-            _db = db;
-
-            if (_db.Lifts.Count() == 0)
-            {
-                _db.Lifts.Add(new Lift { Name = "Bicep Curl" });
-                _db.Lifts.Add(new Lift { Name = "Hammer Curl" });
-                _db.Lifts.Add(new Lift { Name = "Reverse Curl" });
-                _db.Lifts.Add(new Lift { Name = "Bench Press" });
-                _db.Lifts.Add(new Lift { Name = "Incline Bench Press" });
-                _db.SaveChanges();
-            }
         }
 
+        [Authorize]
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Lift>>> GetAllLifts()
+        public IActionResult Get()
         {
-            return await _db.Lifts.ToListAsync();
-        }
-
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Lift>> GetLiftById(long id)
-        {
-            var lift = await _db.Lifts.FindAsync(id);
-
-            if (lift == null)
-            {
-                return NotFound();
-            }
-
-            return lift;
-        }
-
-        [HttpPost]
-        public async Task<ActionResult<Lift>> PostLift(Lift lift)
-        {
-            _db.Lifts.Add(lift);
-            await _db.SaveChangesAsync();
-
-            return CreatedAtAction(nameof(GetLiftById), new { id = lift.Id }, lift);
+            return Content("Restricted resource!");
         }
     }
 }
